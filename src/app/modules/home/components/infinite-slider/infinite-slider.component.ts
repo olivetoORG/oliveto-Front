25a635dd1addd22c78f53496411824f3f45312f4
backend2-abudiyab <backend2-abudiyab>
2023-11-26 10:26:25 +0200

    bannerApi + headerMarginBottom

--- src/app/modules/home/components/infinite-slider/infinite-slider.component.ts
+++ src/app/modules/home/components/infinite-slider/infinite-slider.component.ts
@@ -7,8 +7,21 @@ import { MainServiceService } from 'src/app/services/main-service.service';
   styleUrls: ['./infinite-slider.component.scss'],
 })
 export class InfiniteSliderComponent {
-  constructor(private mainService: MainServiceService) { }
+  constructor(private mainService: MainServiceService) {}
+  bannerData: any[] = [];
 
-
-  
+  ngOnInit(): void {
+    new Promise((resolve) => {
+      this.mainService.getBanner().subscribe({
+        next: (res: any): void => {
+          if (res) {
+            this.bannerData = res?.data;
+            resolve(true);
+            console.log(this.bannerData);
+          }
+        },
+        error: (error) => {},
+      });
+    }).then(() => {});
+  }
 }
